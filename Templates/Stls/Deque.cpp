f88a20da4748c552ad5283cb16813eb6f5b3b132
Ahmad Abdalraheem <Ahmed184417@eng.psu.edu.eg>
2025-08-13 10:46:53 +0300

    Update Deque.cpp

--- Templates/Stls/Deque.cpp
+++ Templates/Stls/Deque.cpp
@@ -9,3 +9,8 @@ dq.pop_front();   // O(1)
 dq.pop_back();    // O(1)
 dq.front();       // O(1)
 dq.back();        // O(1)
+
+// acces elements
+ cout << "First element: " << dq.front() << endl;  // O(1)
+    cout << "Last element: " << dq.back() << endl;    // O(1)
+    cout << "Element at index 2: " << dq[2] << endl;   // O(1)
