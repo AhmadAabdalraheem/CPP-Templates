fa042e7b8da9134f70a03272e5d746229c2fd1e5
Ahmad Abdalraheem <Ahmed184417@eng.psu.edu.eg>
2025-08-13 10:52:10 +0300

    Update and rename Map to Map.cpp

---
+++ Templates/Stls/Map.cpp
@@ -0,0 +1,14 @@
+map< ll , ll > mp;
+// key , value;
+
+//traverse
+// 0 is ignored
+for(auto& it : mp){
+  cout<<it.first() // key 
+    <<' ' << it .second() //value
+    }
+
+for(auto& [key, val] : mp){
+cout<<key<< ' '<<val ;
+}
+
