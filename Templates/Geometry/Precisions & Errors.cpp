dde41a47e0d0fa41c866bbd5637c149c4e8ad360
Ahmad Abdalraheem <Ahmed184417@eng.psu.edu.eg>
2025-08-14 22:16:48 +0300

    Create Precisions & Errors.cpp

---
+++ Templates/Geometry/Precisions & Errors.cpp
@@ -0,0 +1,15 @@
+use EPS for Comparing
+double --> 18 dig , ld --->20 dig
+
+const ld EPS = 1e-9;
+
+ld a = 0 , b =0.00000000001;
+if( a == b ) {
+  مش هيحصل
+}
+fabs--> float abs
+  
+if( fabs(a - b ) < EPS) {
+  تمام
+}
+
