66fa75632ebcd61f4202d6030abe8b1f2282ebeb
Ahmad Abdalraheem <Ahmed184417@eng.psu.edu.eg>
2026-03-14 04:28:08 +0200

    Update SimpleImplementaion.cpp

--- Templates/Graph/DSU/SimpleImplementaion.cpp
+++ Templates/Graph/DSU/SimpleImplementaion.cpp
@@ -7,27 +7,38 @@ using namespace std;
 ;
 ll n ;
 struct DSU {
-    vector<int>parent;
+    vector<int>parent,size_of_component;
     void init(int nn ) {
         parent.resize(nn+1);
+        size_of_component.resize(nn+1);
         for (int i =1 ;i<=nn;i++) {
             parent[i]=i;
+            size_of_component[i] =1 ;
         }
     }
     int find_root(int node) {
             if (node == parent[node]) {
                 return node;
             }
-            return parent[node] = find_root(parent[node]);
+        return parent[node] = find_root(parent[node]);
     }
-    bool merge(int u , int v) {
+    bool merge(int u , int v) { // q log n
 
         int root_u = find_root(u);
         int root_v = find_root(v);
+
         if (root_u == root_v) {
             return false ;
         }
+
+        // merge smaller comp to bigger comp
+        if (size_of_component[root_u]> size_of_component[root_v] ) {
+            swap(root_u,root_v);
+        }
         parent[root_u]= root_v;
+
+
+        size_of_component[root_v] += size_of_component[root_u];
         return true;
     }
 };
