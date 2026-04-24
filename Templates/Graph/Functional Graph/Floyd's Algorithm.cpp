dd1d07b48e9fddf39c461a4f9e4a978296579cb1
Ahmad Abdalraheem <Ahmed184417@eng.psu.edu.eg>
2026-04-24 11:50:50 +0300

    Update Floyd's Algorithm.cpp

--- Templates/Graph/Functional Graph/Floyd's Algorithm.cpp
+++ Templates/Graph/Functional Graph/Floyd's Algorithm.cpp
@@ -7,16 +7,8 @@ const int N = 200005;
 int to[N];
 int n;
 // works from one start node and gets the cycle in this component
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> to[i];
-    }
-
-    int start = 1; // ممكن تغيرها لأي node
+pair<int,int>floyd(int node) {
+    int start = node; // ممكن تغيرها لأي node
 
     int slow = start;
     int fast = start;
@@ -39,12 +31,18 @@ int main() {
     // step 3: find cycle length
     int cycle_length = 1;
     int cur = to[cycle_start];
+    
+    vector<int>path; // the nodes that make the cycle
+    
     while (cur != cycle_start) {
+        path.push_back(cur);
         cur = to[cur];
         cycle_length++;
     }
-
-    cout << cycle_start << " " << cycle_length << '\n';
-
-    return 0;
+    for (auto it : path) {
+        ans[it] = cycle_length;
+        visited[it] =true;
+    }
+    return{ cycle_start, cycle_length};
 }
+
