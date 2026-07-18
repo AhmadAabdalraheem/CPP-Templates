085cc593d58ccae2a86804728405381a473aee4b
Ahmad Abdalraheem <Ahmed184417@eng.psu.edu.eg>
2026-07-18 15:41:53 +0300

    Update NTT.cpp

--- Templates/FFT/NTT.cpp
+++ Templates/FFT/NTT.cpp
@@ -46,20 +46,30 @@ int generator() {
 // In-place NTT loop
 void ntt(vector<int> &a) {
     int n = (int)a.size(), L = 31 - __builtin_clz(n);
+    
     vector<int> rt(2, 1); 
     for (int k = 2, s = 2; k < n; k *= 2, s++) { 
         rt.resize(n);
+        
         int z[] = {1, modpow(root, mod >> s, mod)};
-        for (int i = k; i < 2 * k; ++i) rt[i] = (ll)rt[i / 2] * z[i & 1] % mod;
+        
+        for (int i = k; i < 2 * k; ++i) 
+            rt[i] = (ll)rt[i / 2] * z[i & 1] % mod;
     }
     vector<int> rev(n);
-    for (int i = 0; i < n; ++i) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-    for (int i = 0; i < n; ++i) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int i = 0; i < n; ++i) {
+        rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    }
+    for (int i = 0; i < n; ++i){
+        if (i < rev[i]) swap(a[i], a[rev[i]]);
+    }
     for (int k = 1; k < n; k *= 2) {
         for (int i = 0; i < n; i += 2 * k) {
             for (int j = 0; j < k; ++j) {
                 int z = (ll)rt[j + k] * a[i + j + k] % mod, &ai = a[i + j];
+                
                 a[i + j + k] = ai - z + (z > ai ? mod : 0);
+                
                 ai += (ai + z >= mod ? z - mod : z);
             }
         }
@@ -69,13 +79,21 @@ void ntt(vector<int> &a) {
 // Single-modulus Convolution
 vector<int> conv(const vector<int> &a, const vector<int> &b) {
     if (a.empty() || b.empty()) return {};
+    
     int s = (int)a.size() + (int)b.size() - 1, B = 32 - __builtin_clz(s), n = 1 << B;
+    
     int inv = modpow(n, mod - 2, mod);
+    
     vector<int> L(a), R(b), out(n);
+    
     L.resize(n), R.resize(n);
+    
     ntt(L), ntt(R);
+    
     for (int i = 0; i < n; ++i) out[-i & (n - 1)] = (ll)L[i] * R[i] % mod * inv % mod;
+    
     ntt(out);
+    
     return {out.begin(), out.begin() + s};
 }
 
